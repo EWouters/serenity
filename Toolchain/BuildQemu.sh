78e65ae311fedfa83d553829cc50bb698762c170
EWouters <6179932+EWouters@users.noreply.github.com>
2022-12-16 19:28:36 +0100

    Toolchain: Update BuildQemu.sh to QEMU version 7.2
    QEMU 7.1 was released on December 13th.
    Release Notes: https://wiki.qemu.org/ChangeLog/7.2

--- Toolchain/BuildQemu.sh
+++ Toolchain/BuildQemu.sh
@@ -5,65 +5,65 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "$DIR"
+echo "${DIR}"
 
-PREFIX="$DIR/Local/qemu"
-BUILD=$(realpath "$DIR/../Build")
-SYSROOT="$BUILD/Root"
+PREFIX="${DIR}/Local/qemu"
+BUILD="$(realpath "${DIR}/../Build")"
+SYSROOT="${BUILD}/Root"
 
-QEMU_VERSION=${QEMU_VERSION:="qemu-7.1.0"}
-QEMU_MD5SUM=${QEMU_MD5SUM:="3be5458a9171b4ec5220c65d5d52bdcf"}
+QEMU_VERSION="${QEMU_VERSION:='qemu-7.2.0'}"
+QEMU_MD5SUM="${QEMU_MD5SUM:='7630d6a9eba7ab2bcb9979d6d24c2697'}"
 
-echo PREFIX is "$PREFIX"
-echo SYSROOT is "$SYSROOT"
+echo "PREFIX is ${PREFIX}"
+echo "SYSROOT is ${SYSROOT}"
 
-mkdir -p "$DIR/Tarballs"
+mkdir -p "${DIR}/Tarballs"
 
-pushd "$DIR/Tarballs"
-    md5="$(md5sum $QEMU_VERSION.tar.xz | cut -f1 -d' ')"
-    if [ ! -e "$QEMU_VERSION.tar.xz" ] || [ "$md5" != "$QEMU_MD5SUM" ]; then
-        curl -C - -O "https://download.qemu.org/$QEMU_VERSION.tar.xz"
+pushd "${DIR}/Tarballs"
+    md5="$(md5sum "${QEMU_VERSION}.tar.xz" | cut -f1 -d' ')"
+    if [ ! -e "${QEMU_VERSION}.tar.xz" ] || [ "${md5}" != "${QEMU_MD5SUM}" ]; then
+        curl -C - -O "https://download.qemu.org/${QEMU_VERSION}.tar.xz"
     else
-        echo "Skipped downloading $QEMU_VERSION"
+        echo "Skipped downloading ${QEMU_VERSION}"
     fi
 
-    md5="$(md5sum $QEMU_VERSION.tar.xz | cut -f1 -d' ')"
-    echo "qemu md5='$md5'"
-    if  [ "$md5" != "$QEMU_MD5SUM" ] ; then
-        echo "qemu md5 sum mismatching, please run script again."
-        rm -f $QEMU_VERSION.tar.xz
+    md5="$(md5sum "${QEMU_VERSION}.tar.xz" | cut -f1 -d' ')"
+    echo "qemu md5='${md5}'"
+    if  [ "${md5}" != "${QEMU_MD5SUM}" ] ; then
+        echo 'qemu md5 sum mismatching, please run script again.'
+        rm -f "${QEMU_VERSION}.tar.xz"
         exit 1
     fi
 
-    if [ ! -d "$QEMU_VERSION" ]; then
-        echo "Extracting qemu..."
-        tar -xf "$QEMU_VERSION.tar.xz"
+    if [ ! -d "${QEMU_VERSION}" ]; then
+        echo 'Extracting qemu...'
+        tar -xf "${QEMU_VERSION}.tar.xz"
     else
-        echo "Skipped extracting qemu"
+        echo 'Skipped extracting qemu'
     fi
 
 popd
 
-mkdir -p "$PREFIX"
-mkdir -p "$DIR/Build/qemu"
+mkdir -p "${PREFIX}"
+mkdir -p "${DIR}/Build/qemu"
 
-if [ -z "$MAKEJOBS" ]; then
-    MAKEJOBS=$(nproc)
+if [ -z "${MAKEJOBS}" ]; then
+    MAKEJOBS="$(nproc)"
 fi
 
-if [[ $(uname) == "Darwin" ]]
+if [[ "$(uname)" == 'Darwin' ]]
 then
-    UI_LIB=cocoa
+    UI_LIB='cocoa'
 else
-    UI_LIB=gtk
+    UI_LIB='gtk'
 fi
 
-echo Using $UI_LIB based UI
+echo "Using ${UI_LIB} based UI"
 
-pushd "$DIR/Build/qemu"
-    "$DIR"/Tarballs/$QEMU_VERSION/configure --prefix="$PREFIX" \
-                                            --target-list=aarch64-softmmu,i386-softmmu,x86_64-softmmu \
-                                            --enable-$UI_LIB || exit 1
-    make -j "$MAKEJOBS" || exit 1
+pushd "${DIR}/Build/qemu"
+    "${DIR}/Tarballs/${QEMU_VERSION}/configure" --prefix="${PREFIX}" \
+        --target-list='aarch64-softmmu,i386-softmmu,x86_64-softmmu' \
+        --enable-"${UI_LIB}" || exit 1
+    make -j "${MAKEJOBS}" || exit 1
     make install || exit 1
 popd
