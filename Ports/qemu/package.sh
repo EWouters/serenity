20df035d2aa8afe1d4f7f9c52b827c73c78e518f
EWouters <6179932+EWouters@users.noreply.github.com>
2022-12-17 09:13:39 +0100

    Ports/qemu: Update QEMU to version 7.2.0 [WIP]
    Not compiling at the moment, wants glib-2.56 gthread-2.0.
    
    [skip ci]

--- Ports/qemu/package.sh
+++ Ports/qemu/package.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='qemu'
-version='7.1.0'
+version='7.2.0'
+files="https://download.qemu.org/qemu-${version}.tar.xz qemu-${version}.tar.xz 5b49ce2687744dad494ae90a898c52204a3406e84d072482a1e1be854eeb2157"
+auth_type='sha256'
+depends=(
+    'glib'
+    'pixman'
+    'SDL2'
+)
 useconfigure='true'
 configopts=(
-    "--target-list=aarch64-softmmu,i386-softmmu,x86_64-softmmu"
+    '--target-list=aarch64-softmmu,i386-softmmu,x86_64-softmmu'
     "--cross-prefix=${SERENITY_ARCH}-pc-serenity-"
-    "--extra-ldflags=-lm"
-    "--without-default-features"
-    "--disable-strip"
-    "--enable-pie"
-    "--enable-sdl"
-    "--enable-tcg"
-    "--enable-tools"
+    '--extra-ldflags=-lm'
+    '--without-default-features'
+    '--disable-strip'
+    '--enable-pie'
+    '--enable-sdl'
+    '--enable-tcg'
+    '--enable-tools'
 )
-depends=("glib" "pixman" "SDL2")
-files="https://download.qemu.org/qemu-${version}.tar.xz qemu-${version}.tar.xz a0634e536bded57cf38ec8a751adb124b89c776fe0846f21ab6c6728f1cbbbe6"
-auth_type='sha256'
 
 pre_patch() {
     # Disable tests (those need way more stuff than QEMU itself) by clearing the respective meson file.
@@ -32,4 +36,4 @@ post_install() {
 }
 
 # We don't have '<arch>-pc-serenity-pkg-config', so just use the "normal" one.
-export PKG_CONFIG="pkg-config"
+export PKG_CONFIG='pkg-config'
