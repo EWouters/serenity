5d7fa381cb0594fd7c1d3df5dc487e9b05c28426
EWouters <6179932+EWouters@users.noreply.github.com>
2023-11-17 19:17:36 +0100

    Ports/fio: Update fio to version 3.36

--- Ports/fio/package.sh
+++ Ports/fio/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='fio'
-version='3.33'
+version='3.36'
 files=(
-    "https://brick.kernel.dk/snaps/${port}-${version}.tar.gz#d2410e13e0f379d061d077cc5ae325835bb7c6186aa7bafc1df954cbc9b014fc"
+    "https://brick.kernel.dk/snaps/${port}-${version}.tar.gz#0a07354876ca4d23518f8aa88682f23866455bbd2ff2d0f055d6e4b72f156553"
 )
 depends=(
     'zlib'
