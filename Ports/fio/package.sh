329ed1e9f9baad6a1e54374526a26e408c8bbd60
EWouters <6179932+EWouters@users.noreply.github.com>
2023-11-17 19:13:47 +0100

    Ports: Sadf

--- Ports/fio/package.sh
+++ Ports/fio/package.sh
@@ -4,6 +4,8 @@ version='3.33'
 files=(
     "https://brick.kernel.dk/snaps/${port}-${version}.tar.gz#d2410e13e0f379d061d077cc5ae325835bb7c6186aa7bafc1df954cbc9b014fc"
 )
-depends=("zlib")
+depends=(
+    'zlib'
+)
 
 export LDFLAGS='-ldl'
