a9b83bf7d660a56b194e1aed2bb84f1b13503f5f
EWouters <6179932+EWouters@users.noreply.github.com>
2023-11-17 16:59:07 +0100

    Ports: Sadf

--- Ports/c-ray/package.sh
+++ Ports/c-ray/package.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=c-ray
-version=8f30eb9904a4d20a78e9387d79dc049c5ed69b0c
-useconfigure=true
+port='c-ray'
+version='8f30eb9904a4d20a78e9387d79dc049c5ed69b0c'
+useconfigure='true'
 files=(
     "https://github.com/vkoskiv/c-ray/archive/${version}.tar.gz#27fa6496721faf69f18dc0946f0747b64f3ced748440a8f906f51fcb7e5cb008"
 )
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
-depends=("SDL2")
-workdir="${port}-${version}"
+depends=(
+    'SDL2'
+)
 
 configure() {
-    run cmake "${configopts[@]}"
+    run cmake \
+        -DCMAKE_TOOLCHAIN_FILE="${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
 }
 
 install() {
     mkdir -p "${SERENITY_INSTALL_ROOT}/home/anon/c-ray"
-    cp -r "${port}-${version}"/* "${SERENITY_INSTALL_ROOT}/home/anon/c-ray"
+    cp -r "${workdir}"/* "${SERENITY_INSTALL_ROOT}/home/anon/c-ray"
 }
