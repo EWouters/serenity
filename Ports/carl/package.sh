da9e8222551eb21e752c8bdb671b87671d0d9b64
EWouters <6179932+EWouters@users.noreply.github.com>
2023-11-17 17:16:33 +0100

    Ports/carl: Update carl to version 2.2

--- Ports/carl/package.sh
+++ Ports/carl/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='carl'
-version='1.5'
+version='2.2'
 workdir="cryanc-${version}"
 files=(
-    "https://github.com/classilla/cryanc/archive/refs/tags/${version}.tar.gz#019c2a4df4ce5a332fc29b7903244d6a76bb0bd8bb3e406326b6239416a5b0f6"
+    "https://github.com/classilla/cryanc/archive/refs/tags/${version}.tar.gz#2fa267ab96305d04b6144251f3a0fd7008aecae00aef43be0fb9bc2a75db0a7f"
 )
 
 build() {
