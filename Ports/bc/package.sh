72345aa53152cf3a4daa8647e75200df1ba38c13
EWouters <6179932+EWouters@users.noreply.github.com>
2023-11-17 16:04:51 +0100

    Ports/bc: Update bc to version 6.7.2

--- Ports/bc/package.sh
+++ Ports/bc/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='bc'
-version='6.5.0'
+version='6.7.2'
 files=(
-    "https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz#b1afb1f50c0bce6119c98590bcc8afc22f520bc85c2b512c83938dbb8321cc30"
+    "https://github.com/gavinhoward/bc/releases/download/${version}/bc-${version}.tar.xz#0888d4739caa855c40eab655ae562e900fe7f45a7308ee2feeb95416ec13c7f1"
 )
 useconfigure='true'
 configscript='configure.sh'
