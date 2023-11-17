08a403ed98dcfc49a804e2d1b5da69a739313ef0
EWouters <6179932+EWouters@users.noreply.github.com>
2023-11-17 16:32:40 +0100

    Ports: Sadf

--- Ports/c-ares/package.sh
+++ Ports/c-ares/package.sh
@@ -4,19 +4,18 @@ version='1.19.0'
 files=(
     "https://c-ares.org/download/c-ares-${version}.tar.gz#bfceba37e23fd531293829002cac0401ef49a6dc55923f7f92236585b7ad1dd3"
 )
-useconfigure=true
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt")
+useconfigure='true'
 
 configure() {
     mkdir -p c-ares-build
     cmake -G Ninja \
-        "${configopts[@]}" \
-        -S "$workdir" \
+        -DCMAKE_TOOLCHAIN_FILE="${SERENITY_BUILD_DIR}/CMakeToolchain.txt" \
+        -S "${workdir}" \
         -B c-ares-build
 }
 
 build() {
-    ninja -C c-ares-build "$makeopts"
+    ninja -C c-ares-build "${makeopts}"
 }
 
 install() {
