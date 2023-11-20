c34a4cdcdb15b1fdd096a832127f40062433e951
EWouters <6179932+EWouters@users.noreply.github.com>
2023-11-20 13:11:59 +0100

    Ports/flatbuffers: Update flatbuffers to version 23.5.26

--- Ports/flatbuffers/package.sh
+++ Ports/flatbuffers/package.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 
-port=flatbuffers
-version=2.0.0
+port='flatbuffers'
+version='23.5.26'
 files=(
-    "https://github.com/google/flatbuffers/archive/refs/tags/v${version}.tar.gz#9ddb9031798f4f8754d00fca2f1a68ecf9d0f83dfac7239af1311e4fd9a565c4"
+    "git+https://github.com/google/flatbuffers.git#v${version}"
 )
-useconfigure=true
+useconfigure='true'
 # Since we are cross-compiling, we cannot build the tests, because we need
 # the flatbuffers compiler to build them
-configopts=("-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt" "-DFLATBUFFERS_BUILD_TESTS=off")
+configopts=(
+    "-DCMAKE_TOOLCHAIN_FILE=${SERENITY_BUILD_DIR}/CMakeToolchain.txt"
+    '-DFLATBUFFERS_BUILD_TESTS=off'
+)
 
 configure() {
     run cmake "${configopts[@]}"
