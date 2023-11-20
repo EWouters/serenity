30615a49955499435d60ae2d5b8800f951a7fd85
EWouters <6179932+EWouters@users.noreply.github.com>
2023-11-20 18:15:34 +0100

    [WIP] Ports/gemrb: Update gemrb to version 0.9.2

--- Ports/gemrb/package.sh
+++ Ports/gemrb/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port='gemrb'
-version='0.9.1'
+version='0.9.2'
 useconfigure='true'
 files=(
     "https://github.com/gemrb/gemrb/archive/refs/tags/v${version}.tar.gz#6e5dbcf7398d5566751f434b0d4647196bfbe9a813e3b65ad6a4ee2f1bbfb9ba"
